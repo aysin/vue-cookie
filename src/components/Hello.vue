75ae565b85c69fcbffb1f454267415a8bc7fdc29
Aysin Oruz <aysin.oruz@gmail.com>
2016-12-01 23:12:31 +0100

    More cookie stuff
    :cookie:

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,27 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2>Bird &lt;3 {{ bird }}</h2>
+    <button v-on:click="onClick">{{ counter }}</button>
   </div>
 </template>
 
 <script>
+import cookie from './thibaut'
+console.log(cookie(10))
+
 export default {
   name: 'hello',
-  data () {
+  data: () => {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Challenge Cookie App',
+      bird: 'Cookie',
+      counter: 0
+    }
+  },
+  methods: {
+    onClick: function () {
+      this.counter++
     }
   }
 }
@@ -36,18 +32,12 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+button {
+  font-size: 1.5em;
+  color: inherit;
+  font-family: inherit;
+  border: 1px solid #EEE;
+  background: #FFF;
+  padding:
 }
 </style>
---
+++ src/components/thibaut.js
@@ -0,0 +1,1 @@
+export default bird => bird * 50
