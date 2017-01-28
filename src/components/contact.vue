fe4748e056deb0de93ed65050f522ba66d32e947
Aysin Oruz <aysin.oruz@gmail.com>
2017-01-28 18:08:15 +0100

    Add new images

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <a class="info" href="mailto:team@optimusapp.com"><h4>Contact us: team@optimusapp.com</h4></a> 
+    <a class="info" href="mailto:team@optimusapp.com"><p class="subscription-text">Contact us: team@optimusapp.com</p></a>
     <!-- Begin MailChimp Signup Form -->
     <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
     <div class="subscription-style">
       <form action="//optimusapp.us15.list-manage.com/subscribe/post?u=c111cc44f29731da1dd622cfb&amp;id=ed8567c769" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <p class="subscription-text" for="mce-EMAIL">Want to hear from us?</p>
+        <p class="subscription-text" for="mce-EMAIL">Subscribed to hear from us when the app is released!</p>
         <input type="email" value="" name="EMAIL" class="email"  placeholder="email address" required>
           <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
           <!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c111cc44f29731da1dd622cfb_ed8567c769" tabindex="-1" value=""></div> -->
@@ -34,7 +34,7 @@ export default {
   text-align: center;
 }
 .subscription-style{
-    padding: 10px 30px;
+    /*padding: 10px 30px;*/
     color: #EDEDED;
     text-align: center;
 }
